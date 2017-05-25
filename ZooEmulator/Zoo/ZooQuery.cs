eee28ac8d52117e1481087ff208b5d385f216682
Andriy <andriy.rom1294@gmail.com>
2017-05-25 14:28:39 +0300

    Implemented IQuery as ZooQuery with tests.

---
+++ ZooEmulator/Zoo/ZooQuery.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooEmulator.Animals;
+
+namespace ZooEmulator.Zoo {
+    /// <summary>
+    /// Represent query to IZoo implementors.
+    /// <param name="AnimalType">Animal type to search. If null or empty - search all types.</param>
+    /// <param name="State">Animal state to search. If null search all states.</param>
+    /// </summary>
+    public class ZooQuery : IQuery<IAnimal> {
+        public string AnimalType { get; private set; }
+
+        public Nullable<AnimalStates> State { get; private set; }
+
+        private ZooQuery(string type, Nullable<AnimalStates> state) {
+            AnimalType = type;
+            State = state;
+        }
+
+        public static ZooQuery SearchAll() {
+            return new ZooQuery("", null);
+        }
+
+        public static ZooQuery SearchByType(string type) {
+            return new ZooQuery(type, null); ;
+        }
+
+        public static ZooQuery SearchByState(AnimalStates state) {
+            return new ZooQuery("", state); ;
+        }
+
+        public static ZooQuery Search(string type, AnimalStates state) {
+            return new ZooQuery(type, state);
+        }
+
+        public bool Match(IAnimal element) {
+            bool shouldFilterState = State.HasValue;
+            bool shouldFilterType = !string.IsNullOrEmpty(AnimalType);
+            bool stateFit = shouldFilterState ? element.State.Equals(State.Value) : true;
+            bool typeFit = shouldFilterType ? element.Type.Equals(AnimalType) : true;
+            return stateFit && typeFit;
+        }
+    }
+}
---
+++ ZooEmulatorTest/Zoo/ZooQueryTest.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+using ZooEmulator.Zoo;
+using ZooEmulator.Animals;
+using System.Collections.Generic;
+
+namespace ZooEmulatorTest {
+    [TestClass]
+    public class ZooQueryTest {
+        private const string AnimalTemplate = "Animal{0}_{1}";
+
+        [TestMethod]
+        public void TestSearchAllQuery() {
+            List<IAnimal> animals = GetTestAnimals();
+            IQuery<IAnimal> query = ZooQuery.SearchAll();
+            int resultQueryCount = animals.Where(animal => query.Match(animal)).Count();
+            Assert.AreEqual(resultQueryCount, animals.Count);
+        }
+
+        private List<IAnimal> GetTestAnimals() {
+            var allSpecies = SpeciesData.GetSpeciesInfo();
+            List<IAnimal> firstSpeciesState = GetAnimalAllStates(allSpecies[0]);
+            List<IAnimal> secondSpeciesState = GetAnimalAllStates(allSpecies[1]);
+            return firstSpeciesState.Concat(secondSpeciesState).ToList();
+        }
+
+        private List<IAnimal> GetAnimalAllStates(SpeciesInfo species) {
+            string[] states = Enum.GetNames(typeof(AnimalStates));
+            return states.Select(state => { 
+                string animalName = string.Format(AnimalTemplate, species.Type, state);
+                return new SimpleAnimal(animalName, species.Type, species.MaxHealth) as IAnimal;
+            }).ToList();
+        }
+
+        [TestMethod]
+        public void TestSearchByTypeQuery() {
+            List<IAnimal> animals = GetTestAnimals();
+            string type = animals.First().Type;
+            IQuery<IAnimal> query = ZooQuery.SearchByType(type);
+
+            List<IAnimal> result = animals.Where(animal => query.Match(animal)).ToList();
+            List<IAnimal> animalsLeft = animals.Where(animal => !query.Match(animal)).ToList();
+
+            Assert.IsTrue(result.All(animal => animal.Type == type));
+            Assert.IsTrue(animalsLeft.All(animal => animal.Type != type));
+        }
+
+        [TestMethod]
+        public void TestSearchByStateQuery() {
+            List<IAnimal> animals = GetTestAnimals();
+            AnimalStates state = AnimalStates.Hungry;
+            IQuery<IAnimal> query = ZooQuery.SearchByState(state);
+
+            List<IAnimal> result = animals.Where(animal => query.Match(animal)).ToList();
+            List<IAnimal> animalsLeft = animals.Where(animal => !query.Match(animal)).ToList();
+
+            Assert.IsTrue(result.All(animal => animal.State == state));
+            Assert.IsTrue(animalsLeft.All(animal => animal.State != state));
+        }
+
+        [TestMethod]
+        public void TestSearchByTypeAndStateQuery() {
+            List<IAnimal> animals = GetTestAnimals();
+            string type = animals.First().Type;
+            AnimalStates state = AnimalStates.Hungry;
+            IQuery<IAnimal> query = ZooQuery.Search(type, state);
+
+            List<IAnimal> result = animals.Where(animal => query.Match(animal)).ToList();
+            List<IAnimal> animalsLeft = animals.Where(animal => !query.Match(animal)).ToList();
+
+            Assert.IsTrue(result.All(animal => animal.Type == type && animal.State == state));
+            Assert.IsTrue(animalsLeft.All(animal => animal.Type != type || animal.State != state));
+        }
+    }
+}
