2b7a8d5b351ee33d36612ef187cc7d75cf825188
Andriy <andriy.rom1294@gmail.com>
2017-05-25 13:35:37 +0300

    Added interfaces for Zoo.

---
+++ ZooEmulator/Zoo/IQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZooEmulator.Zoo {
+    public interface IQuery<T> {
+        bool Match(T element);
+    }
+}
---
+++ ZooEmulator/Zoo/IZoo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ZooEmulator.Animals;
+
+namespace ZooEmulator.Zoo {
+    public interface IZoo {
+        List<IAnimal> GetAnimals();
+
+        ExecutionStatus AddAnimal(IAnimal animal);
+
+        ExecutionStatus RemoveAnimal(IAnimal animal);
+
+        IAnimal GetAnimalByName(string name);
+
+        List<IAnimal> QueryAnimals(IQuery<IAnimal> query);
+
+        event EventHandler AllEnimalsDead;
+    }
+}
